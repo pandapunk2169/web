29d375f270143712fe84c9dc8ef4390810909dc7
caohuiboss <2550465923@qq.com>
2023-02-26 21:11:24 +0800

    feat

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
