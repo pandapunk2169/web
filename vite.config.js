d1e8046f376db3d779d0943b0f61eeb2628376ed
TimeLord <40628220+caohuiboss@users.noreply.github.com>
2023-03-14 15:59:34 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './imgs'),
